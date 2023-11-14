28846014eb6367b640fb904843c8b47c51e25dd3
DooHongKm <rlaenghdrlae@naver.com>
2023-11-15 06:50:12 +0900

    Update featureByScore.py & featureBySensitivity.py

--- featureByScore.py
+++ featureByScore.py
@@ -1,26 +1,42 @@
 def modify(dataset, u_data):
     
     for data in dataset:
-        del data['pattern_sensitivity']
+        del data['lifestyle_sensitivity']
         del data['cleaning_sensitivity']
-        del data['food_sensitivity']
-        del data['study_sensitivity']
-        del data['noise_sensitivity']
+        del data['indoor_food_sensitivity']
+        del data['indoor_study_sensitivity']
+        del data['indoor_noise_sensitivity']
         del data['alarm_sensitivity']
-        del data['latenoise_sensitivity']
-        del data['snoring_sensitivity']
+        del data['late_study_sensitivity']
+        del data['sleeping_habit_sensitivity']
         del data['overnight_sensitivity']
         del data['summer_or_winter']
 
-    del u_data['pattern_sensitivity']
+    del u_data['lifestyle_sensitivity']
     del u_data['cleaning_sensitivity']
-    del u_data['food_sensitivity']
-    del u_data['study_sensitivity']
-    del u_data['noise_sensitivity']
+    del u_data['indoor_food_sensitivity']
+    del u_data['indoor_study_sensitivity']
+    del u_data['indoor_noise_sensitivity']
     del u_data['alarm_sensitivity']
-    del u_data['latenoise_sensitivity']
-    del u_data['snoring_sensitivity']
+    del u_data['late_study_sensitivity']
+    del u_data['sleeping_habit_sensitivity']
     del u_data['overnight_sensitivity']
     del u_data['summer_or_winter']
 
     return dataset, u_data
+
+def clustering_modify(dataset):
+    
+    for data in dataset:
+        del data['lifestyle_sensitivity']
+        del data['cleaning_sensitivity']
+        del data['indoor_food_sensitivity']
+        del data['indoor_study_sensitivity']
+        del data['indoor_noise_sensitivity']
+        del data['alarm_sensitivity']
+        del data['late_study_sensitivity']
+        del data['sleeping_habit_sensitivity']
+        del data['overnight_sensitivity']
+        del data['summer_or_winter']
+        
+    return dataset
--- featureBySensitivity.py
+++ featureBySensitivity.py
@@ -1,89 +1,89 @@
 def modify(dataset, u_data):
     
-    pattern_sensitivity = u_data['pattern_sensitivity']
+    lifestyle_sensitivity = u_data['lifestyle_sensitivity']
     cleaning_sensitivity = u_data['cleaning_sensitivity']
-    food_sensitivity = u_data['food_sensitivity']
-    study_sensitivity = u_data['study_sensitivity']
-    noise_sensitivity = u_data['noise_sensitivity']
+    indoor_food_sensitivity = u_data['indoor_food_sensitivity']
+    indoor_study_sensitivity = u_data['indoor_study_sensitivity']
+    indoor_noise_sensitivity = u_data['indoor_noise_sensitivity']
     alarm_sensitivity = u_data['alarm_sensitivity']
-    latenoise_sensitivity = u_data['latenoise_sensitivity']
-    snoring_sensitivity = u_data['snoring_sensitivity']
+    late_study_sensitivity = u_data['late_study_sensitivity']
+    sleeping_habit_sensitivity = u_data['sleeping_habit_sensitivity']
     overnight_sensitivity = u_data['overnight_sensitivity']
-    cold_or_hot = u_data['cold_or_hot']
+    hot_or_cold = u_data['hot_or_cold']
                 
     for data in dataset:
-        data['sleep_score'] = (data['sleep_score'] / 2 + 0.5) * pattern_sensitivity
-        data['wakeup_score'] = (data['wakeup_score'] / 2 + 0.5) * pattern_sensitivity
-        del data['pattern_sensitivity']
+        data['sleep_score'] = (data['sleep_score'] / 2 + 0.5) * lifestyle_sensitivity
+        data['wakeup_score'] = (data['wakeup_score'] / 2 + 0.5) * lifestyle_sensitivity
+        del data['lifestyle_sensitivity']
         data['cleaning_score'] *= cleaning_sensitivity
         del data['cleaning_sensitivity']
-        if food_sensitivity == 1: data['food_score'] = 0
-        elif food_sensitivity == 2: data['food_score'] *= 2
-        elif food_sensitivity == 3: data['food_score'] *= 3
-        del data['food_sensitivity']
-        data['cigarette_score'] *= 3
-        data['study_score'] *= study_sensitivity       
-        del data['study_sensitivity']
-        if noise_sensitivity == 1: data['noise_score'] = 0
-        elif noise_sensitivity == 2: data['noise_score'] *= 2
-        elif noise_sensitivity == 3: data['noise_score'] *= 3
-        del data['noise_sensitivity']
+        if indoor_food_sensitivity == 1: data['indoor_food_score'] = 0
+        elif indoor_food_sensitivity == 2: data['indoor_food_score'] *= 2
+        elif indoor_food_sensitivity == 3: data['indoor_food_score'] *= 3
+        del data['indoor_food_sensitivity']
+        data['smoking_score'] *= 3
+        data['indoor_study_score'] *= indoor_study_sensitivity       
+        del data['indoor_study_sensitivity']
+        if indoor_noise_sensitivity == 1: data['indoor_noise_score'] = 0
+        elif indoor_noise_sensitivity == 2: data['indoor_noise_score'] *= 2
+        elif indoor_noise_sensitivity == 3: data['indoor_noise_score'] *= 3
+        del data['indoor_noise_sensitivity']
         if alarm_sensitivity == 1: data['alarm_score'] = 0
         elif alarm_sensitivity == 2: data['alarm_score'] *= 2
         elif alarm_sensitivity == 3: data['alarm_score'] *= 3
         del data['alarm_sensitivity']
-        if latenoise_sensitivity == 1: data['latenoise_score'] = 0
-        elif latenoise_sensitivity == 2: data['latenoise_score'] *= 2
-        elif latenoise_sensitivity == 3: data['latenoise_score'] *= 3
-        del data['latenoise_sensitivity']
-        if snoring_sensitivity == 1: data['snoring_score'] = 0
-        elif snoring_sensitivity == 2: data['snoring_score'] *= 2
-        elif snoring_sensitivity == 3: data['snoring_score'] *= 3
-        del data['snoring_sensitivity']
-        data['outsider_score'] *= 3
+        if late_study_sensitivity == 1: data['laten_study_score'] = 0
+        elif late_study_sensitivity == 2: data['late_study_score'] *= 2
+        elif late_study_sensitivity == 3: data['late_study_score'] *= 3
+        del data['late_study_sensitivity']
+        if sleeping_habit_sensitivity == 1: data['sleeping_habit_score'] = 0
+        elif sleeping_habit_sensitivity == 2: data['sleeping_habit_score'] *= 2
+        elif sleeping_habit_sensitivity == 3: data['sleeping_habit_score'] *= 3
+        del data['sleeping_habit_sensitivity']
+        data['intimacy_score'] *= 3
         if overnight_sensitivity == 1: data['overnight_score'] = 0
         elif overnight_sensitivity == 2: data['overnight_score'] *= 2
         elif overnight_sensitivity == 3: data['overnight_score'] *= 3
         del data['overnight_sensitivity']
-        if cold_or_hot == 0: data['cold_or_hot'] = 0
-        else: data['cold_or_hot'] *= 3
+        if hot_or_cold == 0: data['hot_or_cold'] = 0
+        else: data['hot_or_cold'] *= 3
         del data['summer_or_winter']
         
-    u_data['sleep_score'] = (u_data['sleep_score'] / 2 + 0.5) * pattern_sensitivity
-    u_data['wakeup_score'] = (u_data['wakeup_score'] / 2 + 0.5) * pattern_sensitivity
-    del u_data['pattern_sensitivity']
+    u_data['sleep_score'] = (u_data['sleep_score'] / 2 + 0.5) * lifestyle_sensitivity
+    u_data['wakeup_score'] = (u_data['wakeup_score'] / 2 + 0.5) * lifestyle_sensitivity
+    del u_data['lifestyle_sensitivity']
     u_data['cleaning_score'] *= cleaning_sensitivity
     del u_data['cleaning_sensitivity']
-    if food_sensitivity == 1: u_data['food_score'] = 0
-    elif food_sensitivity == 2: u_data['food_score'] = 5
-    elif food_sensitivity == 3: u_data['food_score'] = 9
-    del u_data['food_sensitivity']
-    u_data['cigarette_score'] *= 3
-    u_data['study_score'] *= study_sensitivity       
-    del u_data['study_sensitivity']
-    if noise_sensitivity == 1: u_data['noise_score'] = 0
-    elif noise_sensitivity == 2: u_data['noise_score'] = 5
-    elif noise_sensitivity == 3: u_data['noise_score'] = 9
-    del u_data['noise_sensitivity']
+    if indoor_food_sensitivity == 1: u_data['indoor_food_score'] = 0
+    elif indoor_food_sensitivity == 2: u_data['indoor_food_score'] += 3
+    elif indoor_food_sensitivity == 3: u_data['indoor_food_score'] += 6
+    del u_data['indoor_food_sensitivity']
+    u_data['smoking_score'] *= 3
+    u_data['indoor_study_score'] *= indoor_study_sensitivity       
+    del u_data['indoor_study_sensitivity']
+    if indoor_noise_sensitivity == 1: u_data['indoor_noise_score'] = 0
+    elif indoor_noise_sensitivity == 2: u_data['indoor_noise_score'] += 3
+    elif indoor_noise_sensitivity == 3: u_data['indoor_noise_score'] += 6
+    del u_data['indoor_noise_sensitivity']
     if alarm_sensitivity == 1: u_data['alarm_score'] = 0
-    elif alarm_sensitivity == 2: u_data['alarm_score'] = 5
-    elif alarm_sensitivity == 3: u_data['alarm_score'] = 9
+    elif alarm_sensitivity == 2: u_data['alarm_score'] += 3
+    elif alarm_sensitivity == 3: u_data['alarm_score'] += 6
     del u_data['alarm_sensitivity']
-    if latenoise_sensitivity == 1: u_data['latenoise_score'] = 0
-    elif latenoise_sensitivity == 2: u_data['latenoise_score'] = 5
-    elif latenoise_sensitivity == 3: u_data['latenoise_score'] = 9
-    del u_data['latenoise_sensitivity']
-    if snoring_sensitivity == 1: u_data['snoring_score'] = 0
-    elif snoring_sensitivity == 2: u_data['snoring_score'] = 5
-    elif snoring_sensitivity == 3: u_data['snoring_score'] = 9
-    del u_data['snoring_sensitivity']
-    u_data['outsider_score'] *= 3
+    if late_study_sensitivity == 1: u_data['late_study_score'] = 0
+    elif late_study_sensitivity == 2: u_data['late_study_score'] += 3
+    elif late_study_sensitivity == 3: u_data['late_study_score'] += 6
+    del u_data['late_study_sensitivity']
+    if sleeping_habit_sensitivity == 1: u_data['sleeping_habit_score'] = 0
+    elif sleeping_habit_sensitivity == 2: u_data['sleeping_habit_score'] += 3
+    elif sleeping_habit_sensitivity == 3: u_data['sleeping_habit_score'] += 6
+    del u_data['sleeping_habit_sensitivity']
+    u_data['intimacy_score'] *= 3
     if overnight_sensitivity == 1: u_data['overnight_score'] = 0
-    elif overnight_sensitivity == 2: u_data['overnight_score'] = 5
-    elif overnight_sensitivity == 3: u_data['overnight_score'] = 9
+    elif overnight_sensitivity == 2: u_data['overnight_score'] += 3
+    elif overnight_sensitivity == 3: u_data['overnight_score'] += 6
     del u_data['overnight_sensitivity']
-    if cold_or_hot == 0: u_data['cold_or_hot'] = 0
-    else: u_data['cold_or_hot'] *= 3
+    if hot_or_cold == 0: u_data['hot_or_cold'] = 0
+    else: u_data['hot_or_cold'] *= 3
     del u_data['summer_or_winter']
     
     return dataset, u_data
